1bb5d37062bb607142a3fc52576d202610fba02b
Pradeep Kumar <pradeepk@colonial02.seas.gwu.edu>
2017-01-25 17:22:22 -0500

    dfd

--- btree.h
+++ btree.h
@@ -12,11 +12,18 @@
 #define inner_keys 31
 #define inner_values 32
 #define leaf_keys  28
+#define inline_keys 1
 
 #define kinplace_keys 4
 #define kinner_keys 31
 #define kinner_values 32
 #define kleaf_keys  30
+
+#define success 0
+#define keyExist 1 
+#define keyvalueExist 2
+#define splitLeaf	3
+
 
 typedef struct __split_info_t {
 	key_t key;
@@ -25,63 +32,6 @@ typedef struct __split_info_t {
 
 //It seems that order-32/order-64 B-tree is faster than other configuration.
 // due to locality (L1 and prefetching).
-
-class btree_node_t {
-};
-
-
-//768 bytes.. 128 byte for key
-class inner_node_t {
-public:
-    flag_t      flag;    // 4 Byte
-    key_t       key[31]; // 124 Bytes
-    btree_node_t* btree_node[32]; // 256 Byte
-};
-
-//128 bytes
-//bits in key[MSB] has special meaning
-class leaf_node_t {
-public:
-    key_t       key[28]; //60 might be better
-	//XXX
-    value_t*     pvalue;//Many keys will have more than one value.
-    leaf_node_t* next;
-};
-
-class btree_t {
-    private:
-        inner_node_t* inner_node;//the root node.
-        //First leaf node, so that all leaf nodes could be traversed easily.
-        leaf_node_t* leaf_node; 
-
-public:
-    inline void init() {}
-
-    inline value_t 
-    search(key_t key) {return 0;}
-
-    inline status_t
-    insert(key_t key, value_t value)
-    { return 0;}
-
-    inline status_t 
-    insert(pair_t pair)
-    { return 0 ; }
-    
-    inline status_t
-    remove(key_t key, value_t value)
-    { return 0;}
-
-    inline status_t
-    remove(pair_t pair)
-    { return 0;}
-    
-    inline status_t 
-    initial_setup(degree_t degree) 
-    {return 0;};
-    //split
-    //traverse
-};
 
 /**************************/
 //Only Key B-Tree
@@ -121,12 +71,10 @@ public:
 	};
 public:
     inline void init() {}
-
     int search(key_t key); 
     status_t insert(key_t key);
     status_t initial_insert(key_t key) {return 0;};
 
-    
     inline status_t
     remove(key_t key)
     { return 0;}
@@ -135,6 +83,7 @@ public:
 
 private:	
 	void insert_inplace(key_t key);
+	status_t split_inplace(key_t key); 
     status_t split_leaf(kleaf_node_t* leaf_node1, key_t key, split_info_t* split_info);
     status_t split_innernode(kinner_node_t* leaf_node1, int i, split_info_t* split_info);
     void insert_in_leaf(kleaf_node_t* leaf_node1, key_t key);
@@ -142,5 +91,80 @@ private:
 
     //traverse
 };
+
+/**************************************/
+//B-Tree with KV
+//768 bytes.. 128 byte for key
+class inner_node_t {
+public:
+    int8_t      count;
+	int8_t		level;
+	int16_t		unused1;
+	int32_t		unused2;
+    key_t       keys[30]; 
+	inner_node_t* next;
+    void*		values[31];
+};
+
+//128 bytes
+//bits in key[MSB] has special meaning
+class leaf_node_t {
+public:
+	int8_t		count;//5 bits needed
+	int8_t		sorted;//one bit needed
+	int16_t		unused;
+	flag_t		flag; //one bit for each keys
+    key_t       keys[31]; //60 might be better
+	value_t		values[31];//will contain value or an index
+};
+
+class btree_t {
+    private:
+		//number of unique keys in the b-tree
+		degree_t degree;
+
+		//array index in values
+		//if degree is just one and value_count is one as well, means
+		// the pair does not contain value. values contain the value. 
+		int32_t value_count;
+
+		union {
+			inner_node_t* inner_node;//the root node.
+			leaf_node_t*  leaf_node;
+			pair_t		  pair;
+		};
+		kbtree_t** values;
+
+public:
+    inline void init() {}
+
+    inline value_t 
+    search(key_t key) {return 0;}
+
+    status_t insert(key_t key, value_t value);
+    
+    inline status_t
+    remove(key_t key, value_t value)
+    { return 0;}
+
+    
+    inline status_t 
+    initial_setup(degree_t degree) 
+    {return 0;};
+    //split
+    //traverse
+
+private:
+
+	status_t insert_traverse(inner_node_t* root, key_t key, value_t value);
+	status_t insert_in_leaf1(leaf_node_t* leaf_node1, key_t key, value_t value);
+	status_t insert_in_leaf2(leaf_node_t* leaf_node1, key_t key, value_t value, split_info_t* split_info);
+	status_t insert_inplace2(key_t key, value_t value);
+	status_t insert_inplace1(key_t key, value_t value);
+
+	status_t split_leaf(leaf_node_t* leaf_node1, key_t key, value_t value, int i, split_info_t* split_info);
+	status_t split_innernode(inner_node_t* inner_node1, int i, split_info_t* split_info);
+};
+
 
 #endif
