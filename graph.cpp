f306154dbb338b40ca49cc9df0dfcec0ced56786
pradeep-k <pradeepk@gwmail.gwu.edu>
2017-03-20 21:06:50 -0400

    compiles

--- graph.cpp
+++ graph.cpp
@@ -8,3 +8,15 @@ graph::graph()
     p_info = 0;
     p_count = 0;
 }
+
+void p_info_t::batch_update(const string& src, const string& dst)
+{
+}
+    
+void p_info_t::make_graph_baseline()
+{
+}
+
+void p_info_t::store_graph_baseline(string dir)
+{
+}
