7c1f5935df36191a4c3b59e9329d802c553a2285
pradeep-k <pradeepk@gwmail.gwu.edu>
2017-04-08 14:51:48 -0400

    all UT passed, printing lables is a problem

--- test1.cpp
+++ test1.cpp
@@ -123,25 +123,27 @@ void lubm_1()
     query_whereclause qwhere;
     query_clause query;
     
-    query_triplewithfilter qt1;
+    query_triple qt1;
     qt1.set_src("?x", 0);
     qt1.set_pred(pred1);
     qt1.set_dst(dst1);
+    qt1.set_traverse(eTransform);
     qt1.set_query(&query);
 
-    //Get the filter details
-    pid_t type_pid = g->get_pid(pred);
-    pinfo_t *labelgraph = g->p_info[type_pid];
-    univ_t typevalue;
-    if (eOK != g->p_info[0]->get_encoded_value(dst, &typevalue)) {
-        assert(0);
-    }
-    qt1.set_filterobj(labelgraph, typevalue, fn_out);
-    
     qwhere.add_child(&qt1);
-    
     query.add_whereclause(&qwhere);
     query.setup_qid(1);
+    
+    //Get the filter details
+    filter_info_t filter_info;
+    pid_t type_pid = g->get_pid(pred);
+    filter_info.rgraph = g->p_info[type_pid];
+    if (eOK != g->p_info[0]->get_encoded_value(dst, &filter_info.value)) {
+        assert(0);
+    }
+    filter_info.filter_fn = fn_out;
+    srset_t* srset = query.get_srset(0);
+    srset->set_filter(&filter_info);
     
     query.add_selectclause(NULL, 0, 1);
 
@@ -174,33 +176,27 @@ void lubm_4()
     query_whereclause qwhere;
     query_clause query;
     
-    /*
-    query_triple qt;
-    qt.set_src("?x", 0);
-    qt.set_pred(pred);
-    qt.set_dst(dst);
-    qwhere.add_child(&qt);
-    */
-
-    query_triplewithfilter qt1;
+    query_triple qt1;
     qt1.set_src("?x", 0);
     qt1.set_pred(pred1);
     qt1.set_dst(dst1);
+    qt1.set_traverse(eTransform);
     qt1.set_query(&query);
-
-    //Get the filter details
-    pid_t type_pid = g->get_pid(pred);
-    pinfo_t *labelgraph = g->p_info[type_pid];
-    univ_t typevalue;
-    if (eOK != g->p_info[0]->get_encoded_value(dst, &typevalue)) {
-        assert(0);
-    }
-    qt1.set_filterobj(labelgraph, typevalue, fn_out);
     
     qwhere.add_child(&qt1);
-    
     query.add_whereclause(&qwhere);
     query.setup_qid(1);
+    
+    //Get the filter details
+    filter_info_t filter_info;
+    pid_t type_pid = g->get_pid(pred);
+    filter_info.rgraph = g->p_info[type_pid];
+    if (eOK != g->p_info[0]->get_encoded_value(dst, &filter_info.value)) {
+        assert(0);
+    }
+    filter_info.filter_fn = fn_out;
+    srset_t* srset = query.get_srset(0);
+    srset->set_filter(&filter_info);
 
     select_info_t select_info[3];
     select_info[0].name = gstrdup("Y1");
@@ -218,11 +214,10 @@ void lubm_4()
 
 void lubm() 
 {
-    test1();
-    test2();
-    /*lubm_1();
+    //test1();
+    //test2();
+    lubm_1();
     lubm_4();
-    */
     
 }
 
