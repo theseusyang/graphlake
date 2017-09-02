2a5b8acb161b8bfac3ab398b5c1fd1eda5ae965d
pradeep-k <pradeepk@gwmail.gwu.edu>
2017-09-02 10:24:18 -0400

    fixed a bug in PR

--- ext_iterative_analytics.h
+++ ext_iterative_analytics.h
@@ -51,10 +51,12 @@ degree_t* create_degreesnap(vert_table_t<T>* graph, vid_t v_count, snapshot_t* s
         cout << "Huge page alloc failed for degree array" << endl;
         degree_array = (degree_t*)calloc(v_count, sizeof(degree_t));
     }
-
+    #pragma omp parallel
+    {
     snapT_t<T>*   snap_blob = 0;
     vid_t        nebr_count = 0;
-
+    
+    #pragma omp for 
     for (vid_t v = 0; v < v_count; ++v) {
         snap_blob = graph[v].get_snapblob();
         if (0 == snap_blob) { continue; }
@@ -74,11 +76,13 @@ degree_t* create_degreesnap(vert_table_t<T>* graph, vid_t v_count, snapshot_t* s
         degree_array[v] = nebr_count;
         //cout << v << " " << degree_array[v] << endl;
     }
-
+    #pragma omp for
     for (index_t i = old_marker; i < marker; ++i) {
         __sync_fetch_and_add(degree_array + edges[i].src_id, 1);
         __sync_fetch_and_add(degree_array + edges[i].dst_id, 1);
     }
+    }
+
     return degree_array;
 }
 
@@ -86,7 +90,7 @@ template<class T>
 void
 ext_hop1(vert_table_t<T>* graph_out, degree_t* degree_out, 
         snapshot_t* snapshot, index_t marker, edgeT_t<T>* edges,
-        vid_t v_count, sid_t root)
+        vid_t v_count)
 {
     index_t         old_marker = 0;
     index_t         sum = 0;
@@ -95,71 +99,265 @@ ext_hop1(vert_table_t<T>* graph_out, degree_t* degree_out,
         old_marker = snapshot->marker;
     }
     
+    srand(0);
+    int query_count = 64;
+    vid_t* query = (vid_t*)calloc(sizeof(vid_t), query_count); 
+    for (int i = 0; i < query_count; i++) {
+        query[i] = rand()% v_count;;
+    }
+
 	double start = mywtime();
-    
-    sid_t sid = 0;
     degree_t      delta_degree = 0;
     degree_t    durable_degree = 0;
     degree_t        nebr_count = 0;
     degree_t      local_degree = 0;
 
-    vid_t v   = root;
+    sid_t sid = 0;
+    vid_t v   = 0;
     vert_table_t<T>* graph  = graph_out;
     T* adj_list = 0;
     T* local_adjlist = 0;
-    
+
+    for (int i = 0; i < query_count; i++) {
         
-    adj_list = graph[v].get_adjlist();
-    durable_degree = 0;
-    if (0 != adj_list) {
-        durable_degree = get_nebrcount1(adj_list);
-        ++adj_list;
-    }
-    nebr_count = degree_out[v];
-    
-    //traverse the delta adj list
-    delta_degree = nebr_count - durable_degree;
-    delta_adjlist_t<T>* delta_adjlist = graph[v].delta_adjlist;
-    
-    while (delta_adjlist != 0 && delta_degree > 0) {
-        local_adjlist = delta_adjlist->get_adjlist();
-        local_degree = delta_adjlist->get_nebrcount();
-        degree_t i_count = min(local_degree, delta_degree);
-        for (degree_t i = 0; i < i_count; ++i) {
-            sid = get_nebr(local_adjlist, i);
+        v = query[i];
+        nebr_count = degree_out[v];
+        if (0 == nebr_count) continue;
+        
+        adj_list = graph[v].get_adjlist();
+        durable_degree = 0;
+        if (0 != adj_list) {
+            durable_degree = get_nebrcount1(adj_list);
+            ++adj_list;
+        }
+        //traverse the delta adj list
+        delta_degree = nebr_count - durable_degree;
+        delta_adjlist_t<T>* delta_adjlist = graph[v].delta_adjlist;
+        
+        while (delta_adjlist != 0 && delta_degree > 0) {
+            local_adjlist = delta_adjlist->get_adjlist();
+            local_degree = delta_adjlist->get_nebrcount();
+            degree_t i_count = min(local_degree, delta_degree);
+            for (degree_t i = 0; i < i_count; ++i) {
+                sid = get_nebr(local_adjlist, i);
+                sum += sid;
+            }
+            delta_adjlist = delta_adjlist->get_next();
+            delta_degree -= local_degree;
+        }
+        degree_t k_count = min(nebr_count, durable_degree);
+        
+        //traverse the adj list
+        for (vid_t k = 0; k < k_count; ++k) {
+            sid = get_nebr(adj_list, k);
             sum += sid;
         }
-        delta_adjlist = delta_adjlist->get_next();
-        delta_degree -= local_degree;
-    }
-    degree_t k_count = min(nebr_count, durable_degree);
-    
-    //traverse the adj list
-    for (vid_t k = 0; k < k_count; ++k) {
-        sid = get_nebr(adj_list, k);
-        sum += sid;
-    }
 
-    //on-the-fly snapshots should process this
-    vid_t src, dst;
-    index_t sum1 = 0;
-    #pragma omp parallel for reduction(+:sum1)
-    for (index_t i = old_marker; i < marker; ++i) {
-        src = edges[i].src_id;
-        dst = edges[i].dst_id;
-        if (src == v) {
-            sum1 += dst;
+        //on-the-fly snapshots should process this
+        index_t sum1 = 0;
+        #pragma omp parallel 
+        {
+            vid_t src, dst;
+            #pragma omp for reduction(+:sum1)
+            for (index_t j = old_marker; j < marker; ++j) {
+                src = edges[j].src_id;
+                dst = edges[j].dst_id;
+                if (src == v) {
+                    sum1 += dst;
+                }
+
+                if (dst == v) {
+                    sum1 += src;
+                }
+            }
         }
-
-        if (dst == v) {
-            sum1 += src;
-        }
+        sum += sum1;
     }
-    sum += sum1;
-
     double end = mywtime();
 
     cout << "Sum = " << sum << " 1 Hop Time = " << end - start << endl;
+}
+
+template<class T>
+void
+ext_hop2(vert_table_t<T>* graph_out, degree_t* degree_out, 
+        snapshot_t* snapshot, index_t marker, edgeT_t<T>* edges,
+        vid_t v_count)
+{
+    index_t         old_marker = 0;
+
+    if (snapshot) { 
+        old_marker = snapshot->marker;
+    }
+    
+    srand(0);
+    int query_count = 64;
+    vid_t* query = (vid_t*)calloc(sizeof(vid_t), query_count); 
+    for (int i = 0; i < query_count; i++) {
+        query[i] = rand()% v_count;;
+    }
+
+	double start = mywtime();
+    degree_t      delta_degree = 0;
+    degree_t    durable_degree = 0;
+    degree_t        nebr_count = 0;
+    degree_t      local_degree = 0;
+
+    sid_t sid = 0;
+    vid_t v   = 0;
+    vert_table_t<T>* graph  = graph_out;
+    T* adj_list = 0;
+    T* local_adjlist = 0;
+    degree_t d = 0;
+    vid_t* vlist = 0;
+    
+    index_t     sum = 0;
+    index_t    sum1 = 0;
+    index_t    sum2 = 0;
+
+    for (int q = 0; q < query_count; q++) {
+        //first hop------------------
+        d = 0; 
+        v = query[q];
+        nebr_count = degree_out[v];
+        
+        if (nebr_count == 0) { continue; }
+        
+        vlist = (vid_t*)calloc(sizeof(vid_t), nebr_count);
+        
+        
+        adj_list = graph[v].get_adjlist();
+        durable_degree = 0;
+        if (0 != adj_list) {
+            durable_degree = get_nebrcount1(adj_list);
+            ++adj_list;
+        }
+        
+        //traverse the delta adj list
+        delta_degree = nebr_count - durable_degree;
+        delta_adjlist_t<T>* delta_adjlist = graph[v].delta_adjlist;
+        
+        while (delta_adjlist != 0 && delta_degree > 0) {
+            local_adjlist = delta_adjlist->get_adjlist();
+            local_degree = delta_adjlist->get_nebrcount();
+            degree_t i_count = min(local_degree, delta_degree);
+            for (degree_t i = 0; i < i_count; ++i) {
+                sid = get_nebr(local_adjlist, i);
+                vlist[d] = sid;
+                ++d;
+            }
+            delta_adjlist = delta_adjlist->get_next();
+            delta_degree -= local_degree;
+        }
+        degree_t k_count = min(nebr_count, durable_degree);
+        
+        //traverse the adj list
+        for (vid_t k = 0; k < k_count; ++k) {
+            sid = get_nebr(adj_list, k);
+            vlist[d] = sid;
+            ++d;
+        }
+
+        //on-the-fly snapshots should process this
+        #pragma omp parallel
+        { 
+        vid_t src, dst;
+        degree_t d1 = 0;
+        #pragma omp for schedule(static) 
+        for (index_t i = old_marker; i < marker; ++i) {
+            src = edges[i].src_id;
+            dst = edges[i].dst_id;
+            if (src == v) {
+                degree_t d1 = __sync_fetch_and_add(&d, 1);
+                vlist[d1] = dst;
+            }
+
+            if (dst == v) {
+                d1 = __sync_fetch_and_add(&d, 1);
+                vlist[d1] = src;
+            }
+        }
+        }
+    
+        sum = 0;
+        sum1 = 0;
+        //Second hop------------------
+        #pragma omp parallel  
+        {
+        degree_t      delta_degree = 0;
+        degree_t    durable_degree = 0;
+        degree_t        nebr_count = 0;
+        degree_t      local_degree = 0;
+
+        sid_t sid = 0;
+        vid_t vid = 0;
+        vert_table_t<T>* graph  = graph_out;
+        T* adj_list = 0;
+        T* local_adjlist = 0;
+        #pragma omp for schedule (static) reduction(+:sum) nowait
+        for (vid_t j = 0; j < d; ++j) {
+            vid = vlist[j];
+            adj_list = graph[vid].get_adjlist();
+            durable_degree = 0;
+            
+            if (0 != adj_list) {
+                durable_degree = get_nebrcount1(adj_list);
+                ++adj_list;
+            }
+            nebr_count = degree_out[vid];
+            
+            //traverse the delta adj list
+            delta_degree = nebr_count - durable_degree;
+            delta_adjlist_t<T>* delta_adjlist = graph[vid].delta_adjlist;
+            
+            while (delta_adjlist != 0 && delta_degree > 0) {
+                local_adjlist = delta_adjlist->get_adjlist();
+                local_degree = delta_adjlist->get_nebrcount();
+                degree_t i_count = min(local_degree, delta_degree);
+                for (degree_t i = 0; i < i_count; ++i) {
+                    sid = get_nebr(local_adjlist, i);
+                    sum += sid;
+                }
+                delta_adjlist = delta_adjlist->get_next();
+                delta_degree -= local_degree;
+            }
+            degree_t k_count = min(nebr_count, durable_degree);
+            
+            //traverse the adj list
+            for (vid_t k = 0; k < k_count; ++k) {
+                sid = get_nebr(adj_list, k);
+                sum += sid;
+            }
+        }
+
+        //on-the-fly snapshots should process this
+        vid_t src, dst;
+        #pragma omp for schedule (static) reduction(+:sum1)
+        for (index_t i = old_marker; i < marker; ++i) {
+            for (degree_t j = 0;  j < d; ++j) {
+                vid = vlist[j];
+                src = edges[i].src_id;
+                dst = edges[i].dst_id;
+                if (src == vid) {
+                    sum1 += dst;
+                }
+
+                if (dst == vid) {
+                    sum1 += src;
+                }
+            }
+        }
+        }
+        
+        if(vlist) free(vlist);
+        vlist = 0;
+        sum += sum1;
+        sum2 += sum; 
+    }
+
+    double end = mywtime();
+    free(query);
+    cout << "Sum = " << sum2 << " 2 Hop Time = " << end - start << endl;
 }
 
 template<class T>
@@ -350,12 +548,10 @@ ext_pagerank(vert_table_t<T>* graph_in, degree_t* degree_in, degree_t* degree_ou
 {
     index_t old_marker = 0;
 
-    degree_t      delta_degree = 0;
 	float* rank_array = 0 ;
 	float* prior_rank_array = 0;
     float* dset = 0;
 	
-    float	inv_v_count = 0.15;//1.0f/vert_count;
     double start = mywtime();
     
     if (snapshot) { 
@@ -385,8 +581,12 @@ ext_pagerank(vert_table_t<T>* graph_in, degree_t* degree_in, degree_t* degree_ou
     }
 	
 	//initialize the rank, and get the degree information
-    degree_t degree = 0;
     
+    #pragma omp parallel
+    { 
+    degree_t degree = 0;
+    float	inv_v_count = 0.15;//1.0f/vert_count;
+    #pragma omp for
     for (vid_t v = 0; v < v_count; ++v) {
         degree = degree_out[v];
         if (degree != 0) {
@@ -397,6 +597,7 @@ ext_pagerank(vert_table_t<T>* graph_in, degree_t* degree_in, degree_t* degree_ou
             prior_rank_array[v] = 0;
         }
     }
+    }
 
 	//let's run the pagerank
 	for (int iter_count = 0; iter_count < iteration_count; ++iter_count) {
@@ -404,6 +605,7 @@ ext_pagerank(vert_table_t<T>* graph_in, degree_t* degree_in, degree_t* degree_ou
         #pragma omp parallel 
         {
             sid_t sid;
+            degree_t      delta_degree = 0;
             degree_t durable_degree = 0;
             degree_t nebr_count = 0;
             degree_t local_degree = 0;
--- run_test1.sh
+++ run_test1.sh
@@ -12,10 +12,24 @@ do
     array[i]=$((2**$i))
 done 
 
-# page rank
+# 1 hop queries
 for i in 0 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
 do
     for N in 0 1 2 3 4 5
+    do
+        echo "----------------Iteration " $N "-----------------"
+        echo "Shell passed r =" ${array[i]} 
+        echo "./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 11 -o ./del1/ -r" ${array[i]}
+        ./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 12 -o ./del1/ -r ${array[i]}
+        sleep 10
+    done
+    sleep 10
+done
+
+# page rank
+for i in 0 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
+do
+    for N in 0 1 2 3 4 5 6 7 8 9
     do
         echo "----------------Iteration " $N "-----------------"
         echo "Shell passed r =" ${array[i]} 
@@ -29,12 +43,26 @@ done
 # BFS
 for i in  0 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
 do
-    for N in 0 1 2 3 4 5
+    for N in 0 1 2 3 4 5 6 7 
     do
         echo "----------------Iteration " $N "-----------------"
         echo "Shell passed r =" ${array[i]} 
         echo "./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 10 -o ./del1/ -r" ${array[i]}
         ./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 10 -o ./del1/ -r ${array[i]}
+        sleep 10
+    done
+    sleep 10
+done
+
+# 2 hop queries
+for i in 0 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
+do
+    for N in 0 1 2 3 4 5
+    do
+        echo "----------------Iteration " $N "-----------------"
+        echo "Shell passed r =" ${array[i]} 
+        echo "./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 11 -o ./del1/ -r" ${array[i]}
+        ./grdf -i /mnt/disk_huge_1/pradeepk/pradeep_graph/kron_28_16/ -c 3 -j 13 -o ./del1/ -r ${array[i]}
         sleep 10
     done
     sleep 10
