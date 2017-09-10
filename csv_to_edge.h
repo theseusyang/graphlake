925d477ca7fa82989b2b6c506144a93a33b325d2
pradeep-k <pradeepk@gwmail.gwu.edu>
2017-09-10 09:49:17 -0400

    Adding test of weighted graph

--- csv_to_edge.h
+++ csv_to_edge.h
@@ -17,8 +17,10 @@ class plaingraph_manager {
     public:
     static void schema_plaingraph();
     static void schema_plaingraphd();
+    static void schema_weightedgraphd();
     static void setup_graph(vid_t v_count);
     static void prep_graph(const string& idirname, const string& odirname);
     static void prep_graph_sync(const string& idirname, const string& odirname);
     static void prep_graph_paper_num(const string& idirname, const string& odirname);
+    static void prep_weighted_rmat(const string& graph_file, const string& action_file);
 };
--- plain_to_edge.cpp
+++ plain_to_edge.cpp
@@ -84,6 +84,37 @@ void plaingraph_manager::schema_plaingraphd()
     ++p_info;
 }
 
+void plaingraph_manager::schema_weightedgraphd()
+{
+    g->cf_info = new cfinfo_t*[2];
+    g->p_info = new pinfo_t[2];
+    
+    pinfo_t*    p_info    = g->p_info;
+    cfinfo_t*   info      = 0;
+    const char* longname  = 0;
+    const char* shortname = 0;
+    
+    longname = "gtype";
+    shortname = "gtype";
+    g->add_property(longname);
+    p_info->populate_property(longname, shortname);
+    info = new typekv_t;
+    g->add_columnfamily(info);
+    info->create_columns();
+    info->add_column(p_info);
+    ++p_info;
+    
+    longname = "friend";
+    shortname = "friend";
+    g->add_property(longname);
+    p_info->populate_property(longname, shortname);
+    info = new p_dgraph_t;
+    g->add_columnfamily(info);
+    info->create_columns();
+    info->add_column(p_info);
+    ++p_info;
+}
+
 
 void plaingraph_manager::setup_graph(vid_t v_count)
 {
@@ -98,6 +129,69 @@ void plaingraph_manager::setup_graph(vid_t v_count)
     g->file_open(true);
     g->make_graph_baseline();
     g->store_graph_baseline(); 
+}
+
+void plaingraph_manager::prep_weighted_rmat(const string& graph_file, const string& action_file)
+{
+    int fd = open(graph_file.c_str(), O_RDONLY);
+    assert(fd != -1);
+    index_t size = fsize(fd);
+    int64_t* buf = (int64_t*)malloc(size);
+    pread(fd, buf, size, 0);
+    int64_t little_endian = buf[0];
+    assert(little_endian == 0x1234ABCD);
+
+    int64_t nv = buf[1]; 
+    int64_t ne = buf[2];
+    int64_t* v_offset = buf + 3;
+    int64_t* index = v_offset + nv + 1; //+1 for offset in CSR 
+    int64_t* weight = index + ne;
+
+    //Do ingestion
+    propid_t cf_id = g->get_cfid("friend");
+    pgraph_t<lite_edge_t>* graph = (pgraph_t<lite_edge_t>*)g->cf_info[cf_id];
+    edgeT_t<lite_edge_t>   edge;
+    
+    double start = mywtime();
+    for (int64_t v = 0; v < nv + 1; ++v) {
+        for (int64_t i = v_offset[v]; i < v_offset[v+1]; ++i) {
+            edge.src_id = v;
+            edge.dst_id.first = index[i];
+            edge.dst_id.second.value_64b = weight[i];
+            status_t status = graph->batch_edge(edge);
+        }
+    }
+    blog_t<lite_edge_t>* blog = graph->blog;
+    index_t marker = blog->blog_head;
+    cout << "make graph marker = " << marker << endl;
+    index_t snap_marker = 0;
+    if (marker == 0) return;
+
+    graph->create_marker(marker);
+    if (eOK == graph->move_marker(snap_marker)) {
+        graph->make_graph_baseline();
+        graph->store_graph_baseline();
+        g->incr_snapid(snap_marker, snap_marker);
+    }
+    double end = mywtime();
+    cout << "Make graph time = " << end - start << endl;
+
+
+    //Graph Updates
+    int fd1 = open(graph_file.c_str(), O_RDONLY);
+    assert(fd1 != -1);
+    index_t size1 = fsize(fd1);
+    int64_t* buf1 = (int64_t*)malloc(size1);
+    pread(fd1, buf1, size1, 0);
+    int64_t little_endian1 = buf1[0];
+    assert(little_endian1 == 0x1234ABCD);
+
+    int64_t na = buf[1];
+    edge_t* edges = (edge_t*)(buf + 2);
+
+    //Do ingestion
+
+    
 }
 
 void plaingraph_manager::prep_graph_sync(const string& idirname, const string& odirname)
