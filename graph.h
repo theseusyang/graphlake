8c5771bb5cc5245e13e0d0d801e551d2cfe0d714
theseus yang <theseusyang@users.noreply.github.com>
2019-03-21 22:13:01 +0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -48,15 +48,15 @@ class graph {
     map <string, propid_t> str2pid;
     cfinfo_t** cf_info;
     pinfo_t *  p_info;
-    
+    // 批量信息
     batchinfo_t* batch_info;
     batchinfo_t* batch_info1;
-    
+    // 批量大小
     uint8_t      batch_count;
     uint8_t      batch_count1;
     int          cf_count;
     propid_t     p_count;
-
+    // 快照
     snapshot_t*  snapshot;
     string       snapfile;
     FILE*        snap_f;
@@ -77,6 +77,7 @@ class graph {
 
  public:
     graph();
+    // 把图文件写到快照文件中
     inline void set_odir(const string& odir) {
         odirname = odir;
         snapfile = odir + "graph.snap";
@@ -96,22 +97,25 @@ class graph {
     vid_t get_type_vcount(tid_t type);
     tid_t get_total_types();
 	tid_t get_tid(const char* type);
+    // 更新类型	
     sid_t type_update(const string& src, const string& dst);
     sid_t type_update(const string& src, tid_t tid = 0);
     void  type_done();
     sid_t get_sid(const char* src);
     void  type_store(const string& odir);
 
-
+    // 添加列簇
     void add_columnfamily(cfinfo_t* cf, propid_t p_count = 1);
+    // 添加属性
     status_t add_property(const char* longname);
     propid_t get_cfid(propid_t pid);
     propid_t get_cfid(const char* property);
+    // 获得属性
     propid_t get_pid(const char* property);
     
     //queries
     void run_query(query_clause* q);
-    
+    // 批量更新
     status_t batch_update(const string& src, const string& dst, const string& predicate);
     //useful or properties
     status_t batch_update(sid_t src_id, const string& dst, const string& predicate);
@@ -123,15 +127,18 @@ class graph {
     void swap_log_buffer();
     void calc_degree();
     void make_graph_baseline();
+    // 创建快照
     void create_snapshot();
+    // 写入边日志
     void write_edgelog();
     void store_graph_baseline(bool clean = false);
     void read_graph_baseline();
+    // 打开文件
     void file_open(bool trunc);
-    
+    // 创建快照线程
     void create_snapthread();
     static void* snap_func(void* arg);
-
+    // 创建线程
     void create_wthread();
     static void* w_func(void* arg);
 };
